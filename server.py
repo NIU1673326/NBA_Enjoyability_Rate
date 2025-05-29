4c29dc6a0db37c332141d04113140cac79b858bc
Replit Auto <replit@auto.com>
2025-05-29 21:05:04 +0000

    Pre-pull commit

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from threading import Thread
-import main  # Això executa el teu codi principal
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Enjoyability script executat!"
+    return "Enjoyability script is running!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
-Thread(target=run).start()
+if __name__ == "__main__":
+    run()
