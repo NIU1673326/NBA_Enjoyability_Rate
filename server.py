41e8449513e857b42a2bcca86d7ae27d8a8382f1
Replit Auto <replit@auto.com>
2025-05-29 22:44:53 +0000

    Pre-pull commit

--- server.py
+++ server.py
@@ -1,14 +1,11 @@
+# server.py
 from flask import Flask
-from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Enjoyability script is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
+    return "✅ El servidor està actiu!"
 
 if __name__ == "__main__":
-    run()
+    app.run(host='0.0.0.0', port=8080)
